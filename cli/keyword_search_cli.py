322b51f9f57b2adaef0da8d4743fa4039483b9bf
greg-beach <gbeach22@gmail.com>
2025-11-15 19:44:56 -0500

    built an inverted index class to assist with faster searching

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,8 +2,7 @@
 
 import argparse
 
-from lib.keyword_search import search_command
-
+from lib.keyword_search import search_command, build_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -12,6 +11,8 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    search_parser = subparsers.add_parser("build", help="Build the inverted index")
+
     args = parser.parse_args()
 
     match args.command:
@@ -19,7 +20,11 @@ def main() -> None:
             print(f'Searching for: {args.query}')
             results = search_command(args.query)
             for i, res in enumerate(results, 1):
-                print(f"{i}. {res['title']}")
+                print(f"{i}. ({res['id']}) {res['title']}")
+        case "build":
+            print("Building inverted index...")
+            build_command()
+            print("Inverted index built successfully")
         case _:
             parser.print_help()
 
