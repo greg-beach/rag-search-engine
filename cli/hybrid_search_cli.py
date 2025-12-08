c069b42ff6cc285ab6b6c5e52dc4351c5cdd8068
greg-beach <gbeach22@gmail.com>
2025-12-08 12:04:27 -0500

    added expand method to expand user query meaning

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -24,7 +24,7 @@ def main() -> None:
     rrf_parser.add_argument("query", type=str, help="Search query")
     rrf_parser.add_argument("--k",type=int,default=DEFAULT_RRF_K, help="RRF k parameter controlling weight distribution (default=60)")
     rrf_parser.add_argument("--limit", type=int, default=DEFAULT_SEARCH_LIMIT, help="Number of results to return (default=5)")
-    rrf_parser.add_argument("--enhance", type=str, choices=["spell", "rewrite"], help="Query enhancement method")
+    rrf_parser.add_argument("--enhance", type=str, choices=["spell", "rewrite", "expand"], help="Query enhancement method")
     
     args = parser.parse_args()
 
